462877537c4894197018d054e4c0b6e395b6c20e
Abhinav <thakurabhinav17122001@gmail.com>
2021-11-03 17:00:39 +0530

    JWT EXPIRY CHANGED

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
 	return jwt.sign({ id }, process.env.JWT_SECRET, {
-		expiresIn: "2d",
+		expiresIn: process.env.JWT_EXPIRY,
 	});
 };
 
